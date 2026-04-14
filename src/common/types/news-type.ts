9da89822b53384420f403a53e62c6c335f642623
Daryna <daryna2003tk@gmail.com>
2026-04-14 10:00:00 +0200

    add news-type

---
+++ src/common/types/news-type.ts
@@ -0,0 +1,64 @@
+export interface INews {
+    _id?: string;
+    id?: number;
+    title: string;
+    body: string;
+    author: string;
+    date?: string;
+    description?: string;
+    image?: string;
+    category?: string;
+    readTime?: string;
+}
+
+export interface ICreateNewsRequest {
+    title: string;
+    body: string;
+    author: string;
+}
+
+export interface IUpdateNewsRequest {
+    title: string;
+    body: string;
+    author: string;
+}
+
+export interface INewsResponse {
+    _id: string;
+    title: string;
+    body: string;
+    author: string;
+    date: string;
+    __v: number;
+}
+
+export interface IApiNewsResponse<T = unknown> {
+    response?: T;
+    error?: {
+        message: string;
+        status?: number;
+    };
+}
+
+export interface IUseNewsReturn {
+    news: INewsResponse[];
+    currentNews: INewsResponse | null;
+    isLoading: boolean;
+    error: string | null;
+    fetchAllNews: () => Promise<void>;
+    fetchNewsById: (id: string) => Promise<void>;
+    createNewNews: (newsData: ICreateNewsRequest) => Promise<boolean>;
+    updateExistingNews: (
+        id: string,
+        newsData: IUpdateNewsRequest,
+    ) => Promise<boolean>;
+    deleteExistingNews: (id: string) => Promise<boolean>;
+    clearError: () => void;
+}
+
+export interface INewsFormProps {
+    news?: INewsResponse | null;
+    onSubmit: (newsData: ICreateNewsRequest) => Promise<boolean>;
+    onCancel: () => void;
+    isLoading: boolean;
+}
