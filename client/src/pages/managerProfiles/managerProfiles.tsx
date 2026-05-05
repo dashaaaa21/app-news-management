b2063de21c20309c56a8673bef4b42a21078e7fe
Daryna <daryna2003tk@gmail.com>
2026-05-05 14:30:00 +0200

    refactor: move role-based profile pages to client

---
+++ client/src/pages/managerProfiles/managerProfiles.tsx
@@ -0,0 +1,22 @@
+import ProfileHeader from '../../components/sectionProfile/ProfileHeader';
+import UsersTable from '../../components/sectionProfile/UsersTable';
+
+export default function ManagerProfiles() {
+    return (
+        <div className="p-8">
+            <div className="bg-transparent rounded-[36px] border border-black/10">
+                <ProfileHeader
+                    selectedUsers={[]}
+                    onClearSelection={() => {}}
+                    onRefresh={() => {}}
+                    isManager={true}
+                />
+                <UsersTable
+                    selectedUsers={[]}
+                    onSelectedUsersChange={() => {}}
+                    isManager={true}
+                />
+            </div>
+        </div>
+    );
+}
