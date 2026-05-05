867147d1f5ca6e39d80618651a141d0ff243a446
Daryna <daryna2003tk@gmail.com>
2026-05-05 10:00:00 +0200

    refactor: move Users pages to client/src/pages

---
+++ client/src/pages/Users/Users.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import ProfileHeader from '../../components/sectionProfile/ProfileHeader';
+import AddNewButton from '../../components/sectionProfile/AddNewButton';
+import UsersTable from '../../components/sectionProfile/UsersTable';
+
+export default function Users() {
+    const [selectedUsers, setSelectedUsers] = useState<number[]>([]);
+
+    return (
+        <div className="p-8">
+            <div className="bg-transparent rounded-[36px] border border-black/10">
+                <ProfileHeader
+                    selectedUsers={selectedUsers}
+                    onClearSelection={() => setSelectedUsers([])}
+                    onRefresh={() => {}}
+                />
+                <AddNewButton />
+                <UsersTable
+                    selectedUsers={selectedUsers}
+                    onSelectedUsersChange={setSelectedUsers}
+                />
+            </div>
+        </div>
+    );
+}
