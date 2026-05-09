bd57e496dc504bd9a63f3ffbd39f03644dfa0e10
Daryna <daryna2003tk@gmail.com>
2026-05-09 10:00:00 +0200

    chore: remove old Dashboard and layout components from root src

--- src/components/Dashboard/Dashboard.tsx
+++
@@ -1,8 +0,0 @@
-import { DashboardGrid } from '../DashboardGrid/DashboardGrid.tsx';
-export default function Dashboard() {
-    return (
-        <div className="relative min-h-screen p-8">
-            <DashboardGrid />
-        </div>
-    );
-}
