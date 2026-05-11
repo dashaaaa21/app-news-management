641ba830621085d54cf263919fa4d9e13bda641e
Daryna <daryna2003tk@gmail.com>
2026-05-11 10:00:00 +0200

    chore: remove old SectionNews components from root src

--- src/components/sectionNews/NewsHeader.tsx
+++
@@ -1,7 +0,0 @@
-export default function NewsHeader() {
-    return (
-        <div className="flex items-center justify-between">
-            <p className="text-base sm:text-lg font-semibold">All news</p>
-        </div>
-    );
-}
