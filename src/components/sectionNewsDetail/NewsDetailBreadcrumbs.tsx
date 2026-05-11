654f557fc40529071702f305ec7e67a237455ebb
Daryna <daryna2003tk@gmail.com>
2026-05-11 12:00:00 +0200

    chore: remove old SectionNewsDetail components from root src

--- src/components/sectionNewsDetail/NewsDetailBreadcrumbs.tsx
+++
@@ -1,9 +0,0 @@
-export default function NewsDetailBreadcrumbs() {
-    return (
-        <div className="flex items-start gap-2 text-sm text-gray-600 mb-6">
-            <span>All news</span>
-            <span>/</span>
-            <span>News</span>
-        </div>
-    );
-}
