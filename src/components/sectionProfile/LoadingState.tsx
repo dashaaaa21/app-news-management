4686747cd39f7bace98d0a8ce78294a34f4c250e
Daryna <daryna2003tk@gmail.com>
2026-05-11 14:30:00 +0200

    chore: remove old SectionProfile components from root src

--- src/components/sectionProfile/LoadingState.tsx
+++
@@ -1,7 +0,0 @@
-export default function LoadingState() {
-    return (
-        <div className="px-6 py-4">
-            <p className="text-center text-[#65655e]">Loading...</p>
-        </div>
-    );
-}
